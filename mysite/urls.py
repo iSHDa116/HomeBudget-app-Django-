b6b90052b392ab091e7b64931ff261f8fe21e761
Rikit0 <marumochan115@gmail.com>
2025-11-19 13:35:51 +0900

    ログイン画面作成中

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path("", include('homebudget.urls')),
     path("admin/", admin.site.urls),
 ]
